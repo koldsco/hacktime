724f898225aebcb301e625ded4323bba952e5dc7
Maxym Kharchenko <maxym.kharchenko@yahoo.com>
2015-10-07 02:27:57 +0000

    add categories

--- db_api.py
+++ db_api.py
@@ -11,12 +11,13 @@ ES_CONFIG = {
     'path': 'https://search-escloud-ir7f3gyru66ibel5pnp3bdjzoi.us-west-2.es.amazonaws.com/'
 }
 
-def save_to_ES(s3, text):
+def save_to_ES(s3, text, category):
     filter_stop_words = StopWords('stop_words.json')
     body = {
         "file": s3,
         "transcript": text,
-        "text": filter_stop_words.exclude_stopwords(text)
+        "text": filter_stop_words.exclude_stopwords(text),
+        "category": category
     }
     url = '{path}{index}/{type}/{s3}'.format(s3=s3, **ES_CONFIG)
     r = requests.put(url, json=body)
